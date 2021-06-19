97854bc9449dc4a9d7972ea8d532c5977a5e23ea
Rithvik <imt_2020079@iiitm.ac.in>
2021-06-20 00:21:12 +0530

    commit 2

--- Middlewares/seatinfo.js
+++ Middlewares/seatinfo.js
@@ -4,12 +4,13 @@ exports.seatdata = (req, res, next) => {
     
     const { seats } = req.body
     const movieName = req.query.movieName;
-
-    for (let i = 0; i <= seats.length; i++) {
-    
-        client.query(`INSERT INTO SEATDB (moviename , seat ) values ('${movieName}' , '${seats[i]}');`)
+    console.log(seats);
+    for (let i = 0; i < seats.length; i++) {
+        var seatno = seats[i];
+        console.log(seatno)
+        client.query(`INSERT INTO SEATDB (moviename , seat ) values ('${movieName}' , '${seatno}');`);
             
     }
     console.log("Sahi chala")
     next();
-}
+};
